d5810022a7d22541153b30e650a87a2041b6b41d
fjpizarro <fjpizarro@localhost.localdomain>
2022-11-19 21:11:22 +0100

    Funciona sitiosGuardados

--- pages/sitiosGuardados.js
+++ pages/sitiosGuardados.js
@@ -5,29 +5,37 @@ import Sidebar from '@/components/Sidebar';
 import { useSession, getSession } from "next-auth/react"
 import React, { useState, useEffect, useRef } from 'react';
 export default function SitiosGuardados() {
-  let { data: session } = useSession()
+  let { data: session, status } = useSession()
   session = session?.session
   const [places, setPlaces] = useState([]);
-  let loadedAtributtes = useRef(false)
-  useEffect(() => {
-    async function getData() {
-      const saved = await fetch(`http://localhost:3000/api/savedPlaces/` + session.user.email,).then(res => res.json())
-      let aux = []
-      await saved.map(async u => {
-        if(u.userId === session?.user?.email) {
-          const place = await fetch(`http://localhost:3000/api/places/` + u.placeId,).then(res => res.json())
-          aux.push(place)
-        }
-      })
-      setPlaces(aux)
-    }
 
-    if (!loadedAtributtes.current && session != undefined) {
-      getData();
-      loadedAtributtes.current = true
+  useEffect(() => {
+    console.log(status)
+    if (status !== 'authenticated') return
+
+    const getPlaces = async () => {
+      let places = await fetch(`http://localhost:3000/api/savedPlaces/` + session.user.email)
+        .then(res => res.json())
+
+        places = places
+          .filter(p => p.userId === session?.user?.email)
+          .map(item => item.placeId)
+          .map(async id => {
+            const res = await fetch(`http://localhost:3000/api/places/${id}`)
+              .then(res => res.json())
+            return res
+          })
+      
+          
+        Promise.all(places).then(res => {
+          let newPlaces = res.map(item => item)
+          setPlaces(newPlaces)
+        });
     }
+    getPlaces().catch(console.error)
     
-  });
+
+  }, [status])
 
   return (
     <>
@@ -35,7 +43,7 @@ export default function SitiosGuardados() {
       <div className='w-full flex justify-center'>
         <Sidebar />
         <h1 className="w-2/3 h-full capitalize text-white text-2xl text-center">
-          {//console.log(places)
+          {console.log(places)
           }
           {places.map(u => <PlaceCard key={u._id + u.name} title={u.name}
           likes={u.favs} idPlace={u._id} idCity={u.cityId} urlMaps={u.mapsUrl} urlPhotos={u.photoUrl} descp={u.descp} />)}
