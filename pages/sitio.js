2f86463790ccadaa7411af3489534ca22232dbec
Patricia <820115@unizar.es>
2022-11-19 22:42:35 +0100

    sitio con fondo y sidebar

--- pages/sitio.js
+++ pages/sitio.js
@@ -1,28 +1,22 @@
 import Link from "next/link"
 import { FiMapPin } from "react-icons/fi";
 import { FiThumbsUp } from "react-icons/fi";
+import Sidebar from '@/components/Sidebar';
 
 export default function sitio({title,descp,likes,urlMaps,urlImg}) {
     return (
         <>
-        <div className="flex text-black mx-auto my-40 mt-20 bg-grey-200 w-2/3 h-40 rounded-2xl border-2 border-gray-600">
-           <div className="bg-gray-400 w-1/6 h-24 mt-8 ml-8 rounded-2xl">
-                <img src={urlImg} className="mx-auto w-24 h-24 object-fill"/>
-           </div>
+       
+        <div class =" bg-gradient-to-r from-sky-500 to-indigo-500 w-full h-screen w-screen overflow-y-scroll" >
+        <Sidebar/>
+            <div className="fill text-black  bg-white-500 w-screen h-100 rounded-2xl border-2 border-transparent align-baseline"></div>
+            <div  className=" mb-30  ml-70 text-black mx-auto my-40 mt-20 bg-white-500 w-2/3 h-full rounded-2xl border-2 border-white-600"></div>
+           
           
-        </div>
-        <div class="mt-3 p-3 w-full flex justify-center items-center mx-auto w-2/3 h-40 ">
-             <textarea rows="3" class="border p-2 rounded w-full" placeholder="Añada su comentario aquí..."></textarea>
+           
+            
             
         </div>
-        <button
-              type="submit"
-              className=" mx-auto capitalize mt-4 w-full tracking-normal px-4 py-3 text-xs font-bold text-center text-black bg-gray-300 hover:bg-gray-500 rounded-md hover:bg-blue-200"
-              >
-              Añadir
-        </button>
-
-    
        
         </>
     )
